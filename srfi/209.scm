24ac82971edb48a9019ef8c4e500340c6f5a5823
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2022-01-04 18:23:32 -0500

    Implementation: Tighten up enum-set-constructor.

--- srfi/209.scm
+++ srfi/209.scm
@@ -284,7 +284,9 @@
   (let ((type (enum-set-type eset)))
     (lambda (names)
       (list->enum-set type
-                      (map (lambda (sym) (enum-name->enum type sym))
+                      (map (lambda (sym)
+                             (or (enum-name->enum type sym)
+                                 (error "invalid enum name" sym)))
                            names)))))
 
 ;; [Deprecated] Returns a procedure which takes a symbol and returns
